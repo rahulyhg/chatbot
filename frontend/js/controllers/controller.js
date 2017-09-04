efd4ce51e8ed8438eec8c2f86acbda8c468dbba1
pratik <pratik.shah@wohlig.com>
2017-09-04 17:12:35 +0530

    chat link

--- frontend/js/controllers/controller.js
+++ frontend/js/controllers/controller.js
@@ -6,7 +6,7 @@ myApp.controller('HomeCtrl', function ($scope,$rootScope, TemplateService, Navig
         TemplateService.title = "Home"; //This is the Title of the Website
         $scope.navigation = NavigationService.getNavigation();
 
-
+        $scope.uipage = "home";
         $scope.mySlides = [
             'http://flexslider.woothemes.com/images/kitchen_adventurer_cheesecake_brownie.jpg',
             'http://flexslider.woothemes.com/images/kitchen_adventurer_lemon.jpg',
