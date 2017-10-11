31f7cf123cd780390a1b3cba613ff6f59527f08f
pratik <pratik.shah@wohlig.com>
2017-10-11 11:22:40 +0530

    autocomplete

--- config/env/production.js
+++ config/env/production.js
@@ -25,7 +25,7 @@ module.exports = {
    * Set the port in the production environment to 80                        *
    ***************************************************************************/
 
-  port: 8096,
+  port: 8001,
   realHost: "http://104.46.103.162",
   emails: ["chintan@wohlig.com", "jagruti@wohlig.com", "tushar@wohlig.com", "chirag@wohlig.com", "harsh@wohlig.com"]
 
