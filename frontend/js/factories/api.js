22d223d1b675ddc4a1ac6602e58f7c5d1d5ae4a2
Pratik Shah <pratik@wohlig.com>
2017-11-14 18:23:04 +0530

    as

--- frontend/js/factories/api.js
+++ frontend/js/factories/api.js
@@ -3,7 +3,7 @@ myApp.factory('apiService', function ($http, $q, $timeout,CsrfTokenService,$http
     adminurl = "http://104.46.103.162:8097/";
     var adminUrl2 = "http://wohlig.io/api/";
     var adminUrl3 = "http://104.46.103.162:8001/api/"
-    var adminUrl3 = "http://localhost/api/";
+    //var adminUrl3 = "http://localhost/api/";
     //return
     return {
 
