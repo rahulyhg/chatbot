7ecdc05ecafea46a7f866b392e115cb68983143f
pratik <pratik.shah@wohlig.com>
2017-11-20 19:06:45 +0530

    chat ui

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -1,5 +1,55 @@
 module.exports = _.cloneDeep(require("sails-wohlig-controller"));
 var controller = {
+    announce: function(req, res) {
+
+        // Get the socket ID from the reauest
+        var socketId = sails.sockets.getId(req);
+        //console.log(req);
+        // Get the session from the request
+        var session = req.session;
+        var access_role = req.body.query.access_role;
+        var sid = req.body.query.sid;
+        var sname = req.body.query.name;
+        // Create the session.users hash if it doesn't exist already
+        session.users = session.users || {};
+
+        User.create({
+        name: 'unknown',
+        socketId: socketId,
+        access_role: access_role,
+        sid: sid,
+        sname: sname,
+        }).exec(function(err, user) {
+        if (err) {
+            return res.serverError(err);
+        }
+
+        // Save this user in the session, indexed by their socket ID.
+        // This way we can look the user up by socket ID later.
+        session.users[socketId] = user;
+        //console.log(user,"userdata");
+        // Subscribe the connected socket to custom messages regarding the user.
+        // While any socket subscribed to the user will receive messages about the
+        // user changing their name or being destroyed, ONLY this particular socket
+        // will receive "message" events.  This allows us to send private messages
+        // between users.
+        User.subscribe(req, user, 'message');
+
+        // Get updates about users being created
+        User.watch(req);
+
+        // Get updates about rooms being created
+        Room.watch(req);
+
+        // Publish this user creation event to every socket watching the User model via User.watch()
+        User.publishCreate(user, req);
+
+        res.json(user);
+
+        });
+
+
+    },
     loginFacebook: function (req, res) {
         passport.authenticate('facebook', {
             scope: ['public_profile', 'user_friends', 'email'],
